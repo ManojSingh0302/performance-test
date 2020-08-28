4d2f4f8a6854c043a6369a650a72333ab0f6f3cc
Manoj Singh <manoj.singh@upstox.com>
2020-08-28 14:02:38 +0530

    adding requirements.txt and setup.py

---
+++ grpc-load-test-with-locust/locust/load_test_grpc.py
@@ -0,0 +1,92 @@
+#!interpreter [optional-arg]
+# -*- coding: utf-8 -*-
+
+"""
+This contains to load test of Hello World example of gRPC call with locust.
+"""
+
+# Built-in/Generic Imports
+import sys
+import grpc
+import inspect
+import time
+import gevent
+
+# Libs
+from locust.contrib.fasthttp import FastHttpUser
+from locust import task, events, constant
+from locust.runners import STATE_STOPPING, STATE_STOPPED, STATE_CLEANUP, WorkerRunner
+from server.python.helloworld import helloworld_pb2
+from server.python.helloworld import helloworld_pb2_grpc
+
+
+__author__ = 'Manoj Singh'
+
+def stopwatch(func):
+    """To be updated"""
+
+    def wrapper(*args, **kwargs):
+        """To be updated"""
+        # get task's function name
+        previous_frame = inspect.currentframe().f_back
+        _, _, task_name, _, _ = inspect.getframeinfo(previous_frame)
+
+        start = time.time()
+        result = None
+        try:
+            result = func(*args, **kwargs)
+        except Exception as e:
+            total = int((time.time() - start) * 1000)
+            events.request_failure.fire(request_type="TYPE",
+                                        name=task_name,
+                                        response_time=total,
+                                        response_length=0,
+                                        exception=e)
+        else:
+            total = int((time.time() - start) * 1000)
+            events.request_success.fire(request_type="TYPE",
+                                        name=task_name,
+                                        response_time=total,
+                                        response_length=0)
+        return result
+
+    return wrapper
+
+class GRPCMyLocust(FastHttpUser):
+    host = 'http://127.0.0.1:50051'
+    wait_time = constant(0)
+
+    def on_start(self):
+        """ on_start is called when a Locust start before any task is scheduled """
+        pass
+
+    def on_stop(self):
+        """ on_stop is called when the TaskSet is stopping """
+        pass
+
+    @task
+    @stopwatch
+    def grpc_client_task(self):
+        """To be updated"""
+        try:
+            with grpc.insecure_channel('127.0.0.1:50051') as channel:
+                stub = helloworld_pb2_grpc.GreeterStub(channel)
+                response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
+                print(response)
+        except (KeyboardInterrupt, SystemExit):
+            sys.exit(0)
+
+# Stopping the locust if a threshold (in this case the fail ratio) is exceeded
+def checker(environment):
+    while not environment.runner.state in [STATE_STOPPING, STATE_STOPPED, STATE_CLEANUP]:
+        time.sleep(1)
+        if environment.runner.stats.total.fail_ratio > 0.2:
+            print(f"fail ratio was {environment.runner.stats.total.fail_ratio}, quitting")
+            environment.runner.quit()
+            return
+
+
+@events.init.add_listener
+def on_locust_init(environment, **_kwargs):
+    if not isinstance(environment.runner, WorkerRunner):
+        gevent.spawn(checker, environment)
---
+++ grpc-load-test-with-locust/setup.py
@@ -0,0 +1,11 @@
+# Libs
+from setuptools import setup, find_packages
+
+setup(
+    name="api_automation",
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent'],
+    include_package_data=True
+)
